29b47449d1eb48cbb267a5e9f6c088b46c773598
inhun <torihome1@naver.com>
2020-11-26 16:30:14 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, url_for, redirect, request
+from flask_request_params import bind_request_params
+from flask_cors import CORS, cross_origin
+import time
+from datetime import datetime
+import qrcode
+import base64
+from io import BytesIO
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+beverage = ['pepsi', 'demisoda apple', 'georgia max', 'georgia original', 'hot six']
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', beverage=beverage)
+
+
+@app.route('/order', methods=['POST'])
+def order():
+    beverage = request.form['beverages']
+    room = request.form['room']
+    data = {'beverage': beverage, 'label': room}
+    
+    img = qrcode.make(data)
+
+    bufferd = BytesIO()
+    img.save(bufferd, format="JPEG")
+    image_string = base64.b64encode(bufferd.getvalue()).decode('utf-8')
+    return render_template('showqr.html', img_data=image_string)
+    
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
